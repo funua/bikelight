cc7e71e50fb176cb23d01d4102b8867d1e0bd304
funua <funsersver@gmail.com>
2013-02-13 00:20:26 +0200

    fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var mongoose = require('mongoose');
-console.log(mongoose);
+// var mongoose = require('mongoose');
+// console.log(mongoose);
 // mongoose.connect(process.env['MONGOHQ_URL'], 'test');
 
 // var schema = mongoose.Schema({ name: 'string' });
