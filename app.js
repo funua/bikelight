f859e6dc88583a16fdd9500419569af0007c9659
funua <funsersver@gmail.com>
2013-03-27 09:22:27 +0200

    fix

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var express = require('express'),
 
 	mongoose.connect(process.env['MONGOHQ_URL']);
 
-var base = require('./base')(mongoose);
+var schema = require('./schema').init(mongoose),
+	model = require('./model').init(mongoose, schema),
+	auth = require('./auth').init(model.users),
+	controller = require('./controller').init(model),
 	app = express();
 
 app.configure(function(){
@@ -26,13 +29,13 @@ app.configure(function(){
 	app.use(express.errorHandler()); // ONLY FOR DEVELOPMENT	
 });
 
-app.get('/', base.controller);
+app.get('/', controller);
 // admin routes
 app.all('/admin*', function(req, res, next) {
 	if (req.session.user_id && req.session.is_admin) {
 		next()
 	} else {
-		base.auth.tryAuth(req, next, function(err){
+		auth.tryAuth(req, next, function(err){
 			res.render('admin/login.html', {
 				error: errors.get(err),
 				layout: false
@@ -40,15 +43,15 @@ app.all('/admin*', function(req, res, next) {
 		})
 	}
 });
-app.get('/admin/edit_page/:id', base.controller);
-app.post('/admin/edit_page/:id', base.controller);
-app.get('/admin/remove_menu/:id', base.controller);
+app.get('/admin/edit_page/:id', controller);
+app.post('/admin/edit_page/:id', controller);
+app.get('/admin/remove_menu/:id', controller);
 app.post('/admin/update_menu', function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
-	base.controller(req, res, next);
+	controller(req, res, next);
 });
-app.use('/admin', base.controller);
+app.use('/admin', controller);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
